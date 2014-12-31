f6a74c5830c5c40a609ebdf26c832a00d73b9975
Jianru Lin <jianru.lin@gmail.com>
2014-12-31 14:53:08 +0800

    更改了主页标题

--- miaodeli.com/routes/index.js
+++ miaodeli.com/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Miaodeli' });
+  res.render('index', { title: '喵德利/Miaodeli' });
 });
 
 module.exports = router;
